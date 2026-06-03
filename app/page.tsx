d443c23d2bfddb3e702400895d70f5c2bda765df
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-06-03 18:21:24 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -102,7 +102,29 @@ export default function Home() {
     </div>
 
     <h3 className="mt-6 text-2xl font-semibold">
-      Portovenere Experiences
+      Alo's Café
+    </h3>
+
+    <p className="mt-3 text-neutral-600">
+      Shopify e-commerce for motorcycle accessories,
+      gear and premium products.
+    </p>
+  </div>
+
+   {/* Lovely places to stay in Vernazza */}
+  <div className="group overflow-hidden rounded-[32px] bg-white p-6 shadow-sm transition duration-500 hover:-translate-y-1 hover:shadow-xl">
+
+    <div className="relative aspect-[16/10] overflow-hidden rounded-2xl">
+      <Image
+        src="/projects/maria.jpg"
+        alt="Alo's Café"
+        fill
+        className="object-cover transition duration-700 group-hover:scale-105"
+      />
+    </div>
+
+    <h3 className="mt-6 text-2xl font-semibold">
+     Lovely places to stay in Vernazza
     </h3>
 
     <p className="mt-3 text-neutral-600">
