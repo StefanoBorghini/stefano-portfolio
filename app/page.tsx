eb8068219ea482230d905217c5843ac172c3cfe3
Stefano Borghini <projects@stefanoborghinidesign.it>
2026-06-03 19:39:27 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -45,13 +45,24 @@ export default function LoginPage() {
   onSubmit={(e) => {
     e.preventDefault();
     handleLogin();
-  }}>
-        <button
-          onClick={handleLogin}
-          className="mt-6 w-full cursor-pointer rounded-full bg-black px-6 py-4 text-white transition hover:opacity-80"
-        >
-          Enter Preview
-        </button>
+  }}
+>
+  <div className="mt-10">
+    <input
+      type="password"
+      placeholder="Enter password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      className="w-full cursor-text rounded-full border border-neutral-300 bg-white px-6 py-4 text-center text-black placeholder:text-neutral-400 outline-none transition focus:border-black"
+    />
+  </div>
+
+  <button
+    type="submit"
+    className="mt-6 w-full cursor-pointer rounded-full bg-black px-6 py-4 text-white transition hover:opacity-80"
+  >
+    Enter Preview
+  </button>
 </form>
       </div>
     </main>
